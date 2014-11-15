f9d8c2f1bf49b182f6787fb891f582c297f4baee
Gerry Havinga <gerry@linuxcien.nl>
2014-11-15 14:56:29 +0100

    Added .gitignore and updated license and still todo part in script.

--- installAfstandsPiVersion1.0.sh
+++ installAfstandsPiVersion1.0.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Raspberry Pi combined OpenVPN installation script
 # (c) LinuXcien 2014
-# To do:
-# Random account name
-# Random account pass
-# Enable WOL site via script
-# Python + modules installer
-# More secure version of ssmtp
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Still to do:
+#  - Create firewall based on iptables
+#  - Enable WOL site via script
+#  - Python + modules installer
+#  - Mail functionality based on ssmtp/postfix
 
 function switchErrorCheckingOn {
         # Check for variables that are not set (stop script):
